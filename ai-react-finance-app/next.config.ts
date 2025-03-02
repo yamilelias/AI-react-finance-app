80f1509e39f79843089f5430724498a12ebbc5f0
Yamil Elías <yamileliassoto@gmail.com>
2025-03-02 02:49:18 +0000

    init prisma

--- ai-react-finance-app/next.config.ts
+++ ai-react-finance-app/next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
     ignoreDuringBuilds: true
   },
   typescript: {
     ignoreBuildErrors: true
+  },
+  experimental: {
+    serverActions: true
   }
 };
 
